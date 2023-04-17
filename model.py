36797e972a63d6452b65aa28008df24f8c49a258
LiuqianC <852201975@qq.com>
2023-04-17 01:05:08 +0100

    Basic version
    This version has largely completed the software requirements and is able to output the correct images and data.
    Optimisation is still needed in the slider area

--- model.py
+++ model.py
@@ -28,40 +28,57 @@ import modules.geometry as geo
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
-import matplotlib.animation as anim
 import tkinter as tk
 import tkinter.ttk as ttk
-import csv
 
 #%% Functions
-def label(x):
-    
+def labeling(x):
+    '''
+    Purpose: Changing the label which indicates the value of the scale. Changing the factors as the label showing.
+    Hints: When the function first runs, 3 factors are all 0.
+
+    Parameters
+    ----------
+    x : str
+        the value of scale.
+
+    Returns
+    -------
+    None.
+
+    '''
+    # Transfer the 3 global variable
     global geofac, popfac, trafac
     
-    if (round(scale1.get(),2) != geofac):
-        scale1_label.config(text='Geology factor= ' + str(round(float(x)*100,2)) + '%')
-    if (round(scale2.get(),2) != popfac):
-        scale2_label.config(text='Population factor= ' + str(round(float(x)*100,2)) + '%')
-    if (round(scale3.get(),2) != trafac):
-        scale3_label.config(text='Transport factor= ' + str(round(float(x)*100,2)) + '%')
+    # Change the label content, rounded to two decimal places
+    # E.g. Geology factor= 20.34%
+    if (round(scale1.get(),2) != geofac): # when geoslider changes
+        scale1_label.config(text='Geology factor = ' + str(round((float(x)*100))) + '%')
+        scale2.config(to=(1-round(scale1.get(),2)), state="normal")
+    if (round(scale2.get(),2) != popfac): # when popslider changes
+        scale2_label.config(text='Population factor = ' + str(round((float(x)*100))) + '%')
+        scale3.config(from_=(1-round(scale1.get(),2)-round(scale2.get(),2)), to=(1-round(scale1.get(),2)-round(scale2.get(),2)), state="normal")
+        scale3_label.config(text='Transport factor = ' + str(round(((1-float(scale1.get())-float(scale2.get()))*100))) + '%')
     
+    # Change the factors' value to the new ones, rounded to two decimal places
     geofac = round(scale1.get(),2)
     popfac = round(scale2.get(),2)
-    trafac = round(scale3.get(),2)
+    trafac = (1-round(scale1.get(),2)-round(scale2.get(),2))
     
     
 def exiting():
-    try:
-        root.quit()
-        root.destroy()
-    except Exception:
-        # Prevents reporting of a harmless Tcl error message:
-        # "TclError: can't invoke "destroy" command: application has been destroyed"
-        pass
-
+    """
+    Exit the program.
+    """
+    root.quit() # quit root
+    root.destroy() # destory root
+    #sys.exit(0)
+    
 def update(geofac, popfac, trafac):
     """
-    Updates scale_label and canvas.
+    Purpose: Updating scale_label and canvas.
+    Hints: Closing existing figure is to avoid overlaping the new image with the old one.
+           The new figure don't have source data images, so they need to be added again.
 
     Parameters
     ----------
@@ -74,6 +91,8 @@ def update(geofac, popfac, trafac):
 
     """
     
+    # Transfer the global variables
+    global figure, sum_raster
     
     # Calculate the final raster
     sum_raster = [] # create a new list to store the summed value raster data
@@ -84,10 +103,54 @@ def update(geofac, popfac, trafac):
             sum_row.append(result) # append the result to the list sum_row
         sum_raster.append(sum_row) # append the list sum_row to the list sum_raster
     sum_raster = geo.rescale(sum_raster) # rescale the multiplied raster to (0, 255)
+    
+    # Close existing figure
+    plt.close(figure)
+    
+    # Create a new figure
+    figure = plt.figure(figsize=(24, 10))
+    
+    # Change the canva1's figure to the new one
+    canva1.figure = figure
+    
+    # Combine 4 plots into one plot
+    # subplot 1: geology
+    plt.subplot(1, 4, 1) # the overall plot has 1 row and 4 columns and this is the first one
+    plt.imshow(geology) # show list geology as image
+    plt.title('Geology') # the subplot's title
+    
+    # subplot 2: population
+    plt.subplot(1, 4, 2) # this is the second one
+    plt.imshow(population)
+    plt.title('Population')
+    
+    # subplot 3: transport
+    plt.subplot(1, 4, 3) # the third one
+    plt.imshow(transport)
+    plt.title('Transport')
+    
+    #subplot 4: multiplied raster
+    plt.subplot(1, 4, 4) # the fourth one
     plt.imshow(sum_raster)
-    plt.show()
-    #canvas.draw()
+    plt.title('Multiplied Raster')
+    
+    # show the overall plot at canva1
+    canva1.draw()
+    write_button.config(state="normal")
+    return sum_raster
 
+def output():
+    '''
+    Output resulting raster data to a txt file
+    '''
+    try:
+        io.write_data("OutputData/suitability.txt", sum_raster) # output 
+        label = ttk.Label(frame, text="Data has written to local path successfully!") 
+
+    except FileNotFoundError:
+        label = tk.Label(frame, text="ERROR! Please check the output file path")
+    
+    label.grid(row=6, column=1) # show label at (6, 1)
 #%% Main body
 if __name__ == '__main__':
     
@@ -104,77 +167,81 @@ if __name__ == '__main__':
     popfac = 0
     trafac = 0
     
-    #GUI
-    # Initialise source data figure
-    sd_figure = plt.figure(figsize=(18, 10))
-    re_figure = plt.figure(figsize=(18, 10))
-    #ax = figure.add_axes([0, 0, 1, 1])
+    # Create a list to store final raster
+    sum_raster = []
+    
+    # GUI
+    # Initialise figure
+    figure = plt.figure(figsize=(24, 10))
     
     # Create the tkinter window
     root = tk.Tk()
-    root.wm_title("Site Suitability")
+    root.wm_title("Site Suitability") # the window's title
     
     # Create a canvas to display the figure
-    canva1 = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(sd_figure, master=root)
-    canva1._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canva1 = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(figure, master=root) # initialisation
+    canva1._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1) # canva1's position
     
     # Show source data 
     # subplot 1: geology
-    plt.subplot(1, 3, 1)
+    plt.subplot(1, 4, 1) # the overall plot has 1 row and 4 columns and Geology is the first subplot
     plt.imshow(geology)
     plt.title('Geology')
     
     # subplot 2: population
-    plt.subplot(1, 3, 2)
+    plt.subplot(1, 4, 2) # the second is Population
     plt.imshow(population)
     plt.title('Population')
     
     # subplot 3: transport
-    plt.subplot(1, 3, 3)
+    plt.subplot(1, 4, 3) # the third is Transport
     plt.imshow(transport)
     plt.title('Transport')
     
-    plt.tight_layout()  # adjust spacing between subplots
-    
-    plt.show()
+    # Show the plot at canva1
     canva1.draw()    
     
     #Create a container to place sliders and labels
-    frame = ttk.Frame(root)
-    frame.pack()
+    frame = ttk.Frame(root) # in the root
+    frame.pack() # auto placed
     
     # Create geology slider
     # Create a label to describe the variable
-    scale1_name = ttk.Label(frame, text="Geology:")
-    scale1_name.grid(row=0,column=0)
+    scale1_name = ttk.Label(frame, text="Geology:") # text
+    scale1_name.grid(row=0, column=0) # position at (0, 0)
     # Create a scale
-    scale1 = ttk.Scale(frame, from_=0, to=1, command=label, length=500)
-    scale1.grid(row=0,column=1)
+    scale1 = ttk.Scale(frame, from_=0, to=1, command=labeling, length=500) # the scale ranges from 0 to 1; when changing the scale, call function 'label'
+    scale1.grid(row=0,column=1) # position at (0, 1)
     # Create a Label widget to display scale value 
-    scale1_label = ttk.Label(frame, text='Move the scale slider to change the factor.')
-    scale1_label.grid(row=0,column=2)
+    scale1_label = ttk.Label(frame, text='Move the scale slider to change the factor.') #text
+    scale1_label.grid(row=0,column=2) # position at (0, 2)
     
     # Create population slider
+    # Similar to geology slider
     scale2_name = ttk.Label(frame, text="Population:")
     scale2_name.grid(row=1,column=0)
-    scale2 = ttk.Scale(frame, from_=0, to=1, command=label, length=500)
+    scale2 = ttk.Scale(frame, from_=0, to=1, command=labeling, length=500, state="disabled")
     scale2.grid(row=1,column=1)
     scale2_label = ttk.Label(frame, text='Move the scale slider to change the factor.')
     scale2_label.grid(row=1,column=2)
     
     # Create transport slider
+    # Similar to geology slider
     scale3_name = ttk.Label(frame, text="Transport:")
     scale3_name.grid(row=2,column=0)
-    scale3 = ttk.Scale(frame, from_=0, to=1, command=label, length=500)
+    scale3 = ttk.Scale(frame, from_=0, to=1, command=labeling, length=500, state="disabled")
     scale3.grid(row=2,column=1)
     scale3_label = ttk.Label(frame, text='Move the scale slider to change the factor.')
     scale3_label.grid(row=2,column=2)
      
+    # Create a button to generate the multiplied raster (resulting raster)
+    #'command=lambda: update' :lambda be used to transfer CURRENT variable's value, otherwise the initial values are transfered to function 'update'
+    gen_button = ttk.Button(frame, text="Generate suitability raster", command=lambda: update(scale1.get(), scale2.get(), scale3.get())) 
+    gen_button.grid(row=5, column=0) # position at (5, 1)
     
-    button = ttk.Button(frame, text="Generate result raster", command=lambda: update(scale1.get(), scale2.get(), scale3.get()))
-    button.grid(row=5,column=1) # add the button to the window
-    
-    
+    write_button = ttk.Button(frame, text = "Write suitability data", command=lambda: output())
+    write_button.config(state="disabled")
+    write_button.grid(row=5, column=1)
     
     # Create a Button widget and link this with the exiting function
     exit_button = ttk.Button(frame, text="Exit", command=exiting)
